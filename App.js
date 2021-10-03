ca805427b7e8b5d1830538fdcd71eb76677c1d17
Akshaykute12345 <akute7218@gmail.com>
2021-10-04 00:11:53 +0530

    added assignment file

---
+++ App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+import Header from './Assignment2/Header.jsx';
+import Section from './Assignment2/Section.jsx';
+import Footer from './Assignment2/Footer.jsx';
+
+function App() {
+  
+  return (
+    <main className="container" >
+    
+    <Header/> <hr/>
+   
+
+   <Section/>
+   
+
+
+   <Footer/>
+   
+
+   
+
+</main>
+   
+   
+  );
+}
+
+export default App;
